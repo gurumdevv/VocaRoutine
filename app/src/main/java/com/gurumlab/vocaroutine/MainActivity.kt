19d784bb310a6a7e0e3cab8c8ba40037f6c960d6
gurumdevv <gurumdevv@gmail.com>
2023-10-27 18:08:50 +0900

    feat: navigatinon bar 구현 및 단어장 레이아웃 구현

--- app/src/main/java/com/gurumlab/vocaroutine/MainActivity.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/MainActivity.kt
@@ -2,10 +2,22 @@ package com.gurumlab.vocaroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.gurumlab.vocaroutine.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            binding.bottomNavigation.setupWithNavController(it)
+        }
     }
 }
