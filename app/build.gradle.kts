00e9becc64f4141b5b1498c3dc1f55d0c69d1dba
gurumdevv <gurumdevv@gmail.com>
2023-10-27 17:01:23 +0900

    feat: 서버와 통신을 위한 데이터 클래스 및 필요한 기능 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ fun getGoogleClientID(propertyKey: String): String{
 
 dependencies {
 
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
--- app/src/main/java/com/gurumlab/vocaroutine/VocaRoutineApplication.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/VocaRoutineApplication.kt
@@ -2,15 +2,9 @@ package com.gurumlab.vocaroutine
 
 import android.app.Application
 
-class VocaRoutineApplication: Application() {
+class VocaRoutineApplication : Application() {
 
-    private lateinit var dataStore : DataStoreModule
-
-    companion object{
-        private lateinit var application : VocaRoutineApplication
-
-        fun getInstance() : VocaRoutineApplication = application
-    }
+    private lateinit var dataStore: DataStoreModule
 
     override fun onCreate() {
         super.onCreate()
@@ -18,5 +12,12 @@ class VocaRoutineApplication: Application() {
         dataStore = DataStoreModule(this)
     }
 
-    fun getDataStore() : DataStoreModule = dataStore
+    fun getDataStore(): DataStoreModule = dataStore
+
+    companion object {
+        private lateinit var application: VocaRoutineApplication
+
+        fun getInstance(): VocaRoutineApplication = application
+        val appContainer = AppContainer()
+    }
 }
