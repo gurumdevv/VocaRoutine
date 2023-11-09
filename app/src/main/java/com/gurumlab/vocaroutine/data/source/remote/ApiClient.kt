880bb64038dd16e25cf50498893fde0be12367c0
gurumdevv <gurumdevv@gmail.com>
2023-11-09 21:56:02 +0900

    feat: MyListFragment, MyListViewModel, MyListRepository, ApiClient Hilt 적용

--- app/src/main/java/com/gurumlab/vocaroutine/data/source/remote/ApiClient.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/data/source/remote/ApiClient.kt
@@ -1,11 +1,6 @@
 package com.gurumlab.vocaroutine.data.source.remote
 
 import com.gurumlab.vocaroutine.data.model.ListInfo
-import com.squareup.moshi.Moshi
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -18,29 +13,4 @@ interface ApiClient {
 
     @POST("privateList/{uid}.json")
     suspend fun uploadList(@Path("uid") uid: String, @Body newList: ListInfo)
-
-    companion object {
-        private val moshi = Moshi.Builder()
-            .build()
-
-        private const val BASE_URL =
-            "https://vocaroutine-default-rtdb.asia-southeast1.firebasedatabase.app"
-
-        fun create(): ApiClient {
-            val logger = HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            }
-
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logger)
-                .build()
-
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(client)
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .build()
-                .create(ApiClient::class.java)
-        }
-    }
 }
--- app/src/main/java/com/gurumlab/vocaroutine/data/source/remote/MyListRepository.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/data/source/remote/MyListRepository.kt
@@ -3,14 +3,18 @@ package com.gurumlab.vocaroutine.data.source.remote
 import com.gurumlab.vocaroutine.data.model.ListInfo
 import com.gurumlab.vocaroutine.data.source.local.DataStoreModule
 import kotlinx.coroutines.flow.first
+import javax.inject.Inject
 
-class MyListRepository(private val apiClient: ApiClient, private val dataStore: DataStoreModule) {
+class MyListRepository @Inject constructor(
+    private val apiClient: ApiClient,
+    private val dataStore: DataStoreModule
+) {
 
     suspend fun getLists(uid: String): List<ListInfo> {
         return apiClient.getLists(uid).values.toList()
     }
 
     suspend fun getUid(): String {
-        return dataStore.savedUid.first()
+        return dataStore.getUid.first()
     }
 }
