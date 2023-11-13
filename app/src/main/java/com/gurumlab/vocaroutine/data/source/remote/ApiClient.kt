df9fd0c60d6f22a353201cce5d5f0b2fc4780b8b
gurumdevv <gurumdevv@gmail.com>
2023-11-13 17:26:47 +0900

    feat: 필요한 Retrofit 함수 선언

--- app/src/main/java/com/gurumlab/vocaroutine/data/source/remote/ApiClient.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/data/source/remote/ApiClient.kt
@@ -1,16 +1,30 @@
 package com.gurumlab.vocaroutine.data.source.remote
 
 import com.gurumlab.vocaroutine.data.model.ListInfo
+import com.gurumlab.vocaroutine.data.model.SharedListInfo
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiClient {
 
     @GET("privateList/{uid}.json")
     suspend fun getLists(@Path("uid") uid: String): Map<String, ListInfo>
 
+    @GET("sharedList.json")
+    suspend fun getSharedList(): Map<String, SharedListInfo>
+
+    @GET("sharedList.json")
+    suspend fun getSharedListByCreator(
+        @Query("orderBy") orderBy: String,
+        @Query("equalTo") equalTo: String
+    ): Map<String, SharedListInfo>
+
     @POST("privateList/{uid}.json")
     suspend fun uploadList(@Path("uid") uid: String, @Body newList: ListInfo)
+
+    @POST("sharedList.json")
+    suspend fun shareList(@Body newList: SharedListInfo)
 }
