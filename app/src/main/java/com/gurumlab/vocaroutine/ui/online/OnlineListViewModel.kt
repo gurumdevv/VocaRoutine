a5f6bc9037155602f0ddae220ce425361acad341
gurumdevv <gurumdevv@gmail.com>
2023-11-13 22:07:52 +0900

    feat: 단어장 공유화면 MVVM 및 Hilt 적용

---
+++ app/src/main/java/com/gurumlab/vocaroutine/ui/online/OnlineListViewModel.kt
@@ -0,0 +1,27 @@
+package com.gurumlab.vocaroutine.ui.online
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gurumlab.vocaroutine.data.model.SharedListInfo
+import com.gurumlab.vocaroutine.data.source.remote.OnlineListRepository
+import com.gurumlab.vocaroutine.ui.common.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OnlineListViewModel @Inject constructor(private val repository: OnlineListRepository) :
+    ViewModel() {
+
+    private val _items = MutableLiveData<Event<List<SharedListInfo>>>()
+    val item: LiveData<Event<List<SharedListInfo>>> = _items
+
+    fun loadLists() {
+        viewModelScope.launch {
+            val sharedLists = repository.getLists()
+            _items.value = Event(sharedLists)
+        }
+    }
+}
