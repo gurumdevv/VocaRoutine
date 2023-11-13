68023e5a2bc634245eb410b1736d18a9b24a4dae
gurumdevv <gurumdevv@gmail.com>
2023-11-13 21:17:55 +0900

    feat: 단어장 공유 상세 화면에 접근할 수 있는 ClickListener 구현

--- app/src/main/java/com/gurumlab/vocaroutine/ui/online/OnlineListAdapter.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/ui/online/OnlineListAdapter.kt
@@ -8,38 +8,37 @@ import com.gurumlab.vocaroutine.R
 import com.gurumlab.vocaroutine.data.model.ListInfo
 import com.gurumlab.vocaroutine.data.model.SharedListInfo
 import com.gurumlab.vocaroutine.databinding.ItemOnlineListBinding
+import com.gurumlab.vocaroutine.ui.common.ListClickListener
 import com.gurumlab.vocaroutine.ui.common.SharedListDiffUtil
 
-class OnlineListAdapter :
+class OnlineListAdapter(private val clickListener: ListClickListener) :
     ListAdapter<SharedListInfo, OnlineListAdapter.OnlineListViewHolder>(SharedListDiffUtil()) {
 
     override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
+        parent: ViewGroup, viewType: Int
     ): OnlineListViewHolder {
         return OnlineListViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: OnlineListViewHolder, position: Int) {
-        holder.bind(getItem(position).listInfo)
+        holder.bind(getItem(position).listInfo, clickListener)
     }
 
     class OnlineListViewHolder(private val binding: ItemOnlineListBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(list: ListInfo) {
+        fun bind(list: ListInfo, clickListener: ListClickListener) {
             binding.tvListTitle.text = list.title
             binding.tvTotalCount.text =
                 binding.root.context.getString(R.string.total_count, list.totalCount)
+            itemView.setOnClickListener { clickListener.onClick(list) }
         }
 
         companion object {
             fun from(parent: ViewGroup): OnlineListViewHolder {
                 return OnlineListViewHolder(
                     ItemOnlineListBinding.inflate(
-                        LayoutInflater.from(parent.context),
-                        parent,
-                        false
+                        LayoutInflater.from(parent.context), parent, false
                     )
                 )
             }
