2dbfd18e9c3e7b4c0d1eb0cb33352a3f618d35c4
gurumdevv <gurumdevv@gmail.com>
2023-11-17 23:23:50 +0900

    refactor: AfterPhotoFragment MVVM, Hilt 적용

--- app/src/main/java/com/gurumlab/vocaroutine/ui/making/AfterPhotoFragment.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/ui/making/AfterPhotoFragment.kt
@@ -9,23 +9,19 @@ import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.gurumlab.vocaroutine.R
-import com.gurumlab.vocaroutine.data.model.TempListInfo
-import com.gurumlab.vocaroutine.data.model.Vocabulary
 import com.gurumlab.vocaroutine.data.source.local.DataStoreModule
 import com.gurumlab.vocaroutine.databinding.FragmentAfterPhotoBinding
 import com.gurumlab.vocaroutine.ui.BaseFragment
 import com.gurumlab.vocaroutine.ui.common.Constants
+import com.gurumlab.vocaroutine.ui.common.EventObserver
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -34,8 +30,7 @@ class AfterPhotoFragment : BaseFragment<FragmentAfterPhotoBinding>() {
     @Inject
     lateinit var dataStore: DataStoreModule
     private lateinit var uid: String
-    private val vocabularies = mutableListOf<Vocabulary>()
-    private var totalCount = 0
+    private val viewModel by viewModels<AfterPhotoViewModel>()
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,41 +49,13 @@ class AfterPhotoFragment : BaseFragment<FragmentAfterPhotoBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding!!.viewModel = viewModel
+        binding!!.lifecycleOwner = viewLifecycleOwner
 
-        setResultLauncher()
-        val intent = Intent(requireContext(), ScanActivity::class.java)
-        resultLauncher.launch(intent)
-
-        binding!!.topAppBar.setOnClickListener {
-            findNavController().navigateUp()
-        }
-
-        binding!!.btnNext.setOnClickListener {
-            createVocabulary()
-        }
-
-        binding!!.btnDone.setOnClickListener {
-            createVocabulary()
-
-            if (vocabularies.isEmpty()) {
-                Snackbar.make(requireView(), getString(R.string.empty_list), Snackbar.LENGTH_SHORT)
-                    .show()
-            } else {
-                val date = getCurrentTimeAsString()
-                val alarmCode = getAlarmCode()
-                if (alarmCode == 0) {
-                    findNavController().navigateUp()
-                }
-
-                val tempListInfo =
-                    TempListInfo(date, totalCount, 0, false, alarmCode, vocabularies.toList())
-
-                val action =
-                    AfterPhotoFragmentDirections.actionAfterPhotoToDialog(uid, tempListInfo)
-                findNavController().navigate(action)
-            }
-        }
-
+        startScanActivity()
+        setTopAppBar()
+        setObservers()
+        setBtnNextClickListener()
         hideBottomNavigation(false)
     }
 
@@ -102,53 +69,10 @@ class AfterPhotoFragment : BaseFragment<FragmentAfterPhotoBinding>() {
         hideBottomNavigation(true)
     }
 
-    private fun createVocabulary() {
-        val word = binding!!.tvVocabulary.text ?: ""
-        val meaning = binding!!.tvMeaning.text ?: ""
-
-        if (word.isNotEmpty() && meaning.isNotEmpty()) {
-            val vocabulary = Vocabulary(word.toString(), meaning.toString(), "etymology")
-            vocabularies.add(vocabulary)
-            totalCount++
-
-            binding!!.tvVocabulary.setText("")
-            binding!!.tvMeaning.setText("")
-        } else {
-            Snackbar.make(
-                requireView(),
-                getString(R.string.fill_in_blank_word),
-                Snackbar.LENGTH_SHORT
-            )
-                .show()
-        }
-    }
-
-    private fun getCurrentTimeAsString(): String {
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
-        val currentTime = Date()
-        return dateFormat.format(currentTime)
-    }
-
-    private fun getAlarmCode(): Int {
-        val calendar = Calendar.getInstance()
-        val currentTIme = Date()
-        calendar.time = currentTIme
-
-        val year = calendar.get(Calendar.YEAR).toString().takeLast(2).toInt()
-        val month = calendar.get(Calendar.MONTH) + 1
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-        val hour = calendar.get(Calendar.HOUR_OF_DAY)
-        val minute = calendar.get(Calendar.MINUTE)
-
-        return try {
-            "${year * month * day}${hour}${minute}0".toInt()
-        } catch (e: NumberFormatException) {
-            Snackbar.make(
-                binding!!.root.rootView,
-                getString(R.string.alarm_code_creation_error, e), Snackbar.LENGTH_LONG
-            ).show()
-            0
-        }
+    private fun startScanActivity() {
+        setResultLauncher()
+        val intent = Intent(requireContext(), ScanActivity::class.java)
+        resultLauncher.launch(intent)
     }
 
     private fun setResultLauncher() {
@@ -156,10 +80,60 @@ class AfterPhotoFragment : BaseFragment<FragmentAfterPhotoBinding>() {
             ActivityResultContracts.StartActivityForResult()
         ) { result ->
             if (result.resultCode == RESULT_OK) {
-
                 val ocrResult = result.data?.getStringExtra(Constants.KEY_OCR_RESULT) ?: ""
                 binding!!.etOcrResult.setText(ocrResult)
             }
+        }
+    }
+
+    private fun setObservers() {
+        viewModel.isCompleted.observe(viewLifecycleOwner, EventObserver { isCompleted ->
+            if (isCompleted) {
+                binding!!.tvVocabulary.setText("")
+                binding!!.tvMeaning.setText("")
+
+                binding!!.btnNext.text = getString(R.string.next)
+            } else {
+                binding!!.btnNext.text = getString(R.string.loading_etymology_now)
+            }
+        })
+
+        viewModel.alarmCode.observe(viewLifecycleOwner, EventObserver { alarmCode ->
+            if (alarmCode == 0) {
+                findNavController().navigateUp()
+            }
+        })
+
+        viewModel.tempList.observe(viewLifecycleOwner, EventObserver { tempListInfo ->
+            val action =
+                AfterPhotoFragmentDirections.actionAfterPhotoToDialog(uid, tempListInfo)
+            findNavController().navigate(action)
+        })
+
+        viewModel.snackbarText.observe(viewLifecycleOwner, EventObserver { messageId ->
+            Snackbar.make(requireView(), getString(messageId), Snackbar.LENGTH_SHORT)
+                .setAnchorView(binding!!.btnDone)
+                .show()
+        })
+
+        viewModel.numberOfAttempts.observe(viewLifecycleOwner, EventObserver { count ->
+            if (count >= 2) {
+                viewModel.setErrorMessage(getString(R.string.gpt_response_error))
+            }
+        })
+    }
+
+    private fun setBtnNextClickListener() {
+        binding!!.btnNext.setOnClickListener {
+            lifecycleScope.launch {
+                viewModel.createVocabulary()
+            }
+        }
+    }
+
+    private fun setTopAppBar() {
+        binding!!.topAppBar.setNavigationOnClickListener {
+            findNavController().navigateUp()
         }
     }
 
---
+++ app/src/main/java/com/gurumlab/vocaroutine/ui/making/AfterPhotoViewModel.kt
@@ -0,0 +1,135 @@
+package com.gurumlab.vocaroutine.ui.making
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gurumlab.vocaroutine.R
+import com.gurumlab.vocaroutine.data.model.TempListInfo
+import com.gurumlab.vocaroutine.data.model.Vocabulary
+import com.gurumlab.vocaroutine.data.source.remote.MakingListRepository
+import com.gurumlab.vocaroutine.ui.common.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.lang.NumberFormatException
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import javax.inject.Inject
+
+@HiltViewModel
+class AfterPhotoViewModel @Inject constructor(private val repository: MakingListRepository) :
+    ViewModel() {
+
+    val word = MutableLiveData<String>()
+    val meaning = MutableLiveData<String>()
+    private val etymology = MutableLiveData<String>()
+    private val _alarmCode = MutableLiveData<Event<Int>>()
+    val alarmCode: LiveData<Event<Int>> = _alarmCode
+    private val _tempList = MutableLiveData<Event<TempListInfo>>()
+    val tempList: LiveData<Event<TempListInfo>> = _tempList
+    private val _snackbarText = MutableLiveData<Event<Int>>()
+    val snackbarText: LiveData<Event<Int>> = _snackbarText
+    private val _isCompleted = MutableLiveData<Event<Boolean>>()
+    val isCompleted = _isCompleted
+    private val _numberOfAttempts = MutableLiveData<Event<Int>>()
+    val numberOfAttempts = _numberOfAttempts
+    private val vocabularies = mutableListOf<Vocabulary>()
+
+    suspend fun createVocabulary() {
+        val currentWord = word.value ?: ""
+        val currentMeaning = meaning.value ?: ""
+        if (!isValidValue(currentWord, R.string.fill_in_blank_word) ||
+            !isValidValue(currentMeaning, R.string.fill_in_blank_meaning)
+        ) return
+
+        _isCompleted.value = Event(false)
+
+        etymology.value = repository.getEtymology(currentWord)
+
+        var count = 0
+        numberOfAttempts.value = Event(count)
+
+        while (etymology.value == "error") {
+            etymology.value = repository.getEtymology(currentWord)
+
+            count++
+            numberOfAttempts.value = Event(count)
+            if (count >= 2) {
+                break
+            }
+        }
+
+        val vocabulary = Vocabulary(currentWord, currentMeaning, etymology.value!!)
+        vocabularies.add(vocabulary)
+        _isCompleted.value = Event(true)
+    }
+
+    fun createList() {
+        viewModelScope.launch {
+            createVocabulary()
+
+            if (vocabularies.isEmpty()) {
+                _snackbarText.value = Event(R.string.empty_list)
+            } else {
+                val totalCount = vocabularies.size
+                val date = getCurrentTime()
+                val currentAlarmCode = getAlarmCode()
+                _alarmCode.value = Event(currentAlarmCode)
+
+                if (alarmCode.value!!.content == 0) {
+                    _snackbarText.value = Event(R.string.alarm_code_creation_error)
+                }
+
+                _tempList.value =
+                    Event(
+                        TempListInfo(
+                            date,
+                            totalCount,
+                            0,
+                            false,
+                            currentAlarmCode,
+                            vocabularies.toList()
+                        )
+                    )
+            }
+        }
+    }
+
+    private fun getCurrentTime(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        val currentTime = Date()
+        return dateFormat.format(currentTime)
+    }
+
+    private fun getAlarmCode(): Int {
+        val calendar = Calendar.getInstance()
+        val currentTIme = Date()
+        calendar.time = currentTIme
+
+        val year = calendar.get(Calendar.YEAR).toString().takeLast(2).toInt()
+        val month = calendar.get(Calendar.MONTH) + 1
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        return try {
+            "${year * month * day}${hour}${minute}0".toInt()
+        } catch (e: NumberFormatException) {
+            0
+        }
+    }
+
+    fun setErrorMessage(message: String) {
+        etymology.value = message
+    }
+
+    private fun isValidValue(value: String, messageId: Int): Boolean {
+        if (value.isBlank()) {
+            _snackbarText.value = Event(messageId)
+            return false
+        }
+        return true
+    }
+}
