835029a94075dd975af037f1a01815cec300cd37
gurumdevv <gurumdevv@gmail.com>
2023-11-25 20:38:01 +0900

    feat: 알람 스케쥴 관리를 위한 함수 추가 및 데이터 구조 변경

--- app/src/main/java/com/gurumlab/vocaroutine/data/source/local/AlarmDao.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/data/source/local/AlarmDao.kt
@@ -17,6 +17,15 @@ interface AlarmDao {
     @Query("DELETE FROM activeAlarms WHERE alarmCode = :alarmCode")
     suspend fun deleteAlarm(alarmCode: Int)
 
+    @Query("DELETE FROM activeAlarms WHERE (id = :id) AND (date LIKE :date || '%')")
+    suspend fun deleteAlarmFromIdAndDate(id: String, date: String)
+
+    @Query("DELETE FROM activeAlarms WHERE date < (:today || '%')")
+    suspend fun deleteOutOfDateAlarms(today: String)
+
     @Query("SELECT alarmCode FROM activeAlarms WHERE alarmCode IN (:alarmCodes)")
     suspend fun searchActiveAlarms(alarmCodes: IntArray): List<Int>
+
+    @Query("SELECT id FROM activeAlarms WHERE date LIKE :date || '%'")
+    suspend fun searchListByDate(date: String): List<String>
 }
