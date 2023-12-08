9acbca39f06f65b239ed4792f0ad4b0f8f3a638c
gurumdevv <gurumdevv@gmail.com>
2023-12-08 10:59:26 +0900

    refactor: Romm Database에 저장할 수 있도록 어노테이션 추가

--- app/src/main/java/com/gurumlab/vocaroutine/data/model/ListInfo.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/data/model/ListInfo.kt
@@ -1,11 +1,14 @@
 package com.gurumlab.vocaroutine.data.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
+@Entity
 @Parcelize
 data class ListInfo(
-    val id: String,
+    @PrimaryKey val id: String,
     val title: String,
     val creator: String,
     val createdDate: String,
