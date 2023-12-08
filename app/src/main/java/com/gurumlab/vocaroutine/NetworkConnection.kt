4be71eb49c126bcf8d66ecb9c0fde65f6149cc6b
gurumdevv <gurumdevv@gmail.com>
2023-12-08 11:03:37 +0900

    feat: connectivityManager을 이용해서 온/오프라인 상태를 알려주는 클래스 구현

---
+++ app/src/main/java/com/gurumlab/vocaroutine/NetworkConnection.kt
@@ -0,0 +1,54 @@
+package com.gurumlab.vocaroutine
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkCapabilities
+import androidx.lifecycle.LiveData
+import com.gurumlab.vocaroutine.ui.common.Event
+
+class NetworkConnection(context: Context) : LiveData<Event<Boolean>>() {
+    private val connectivityManager = context.getSystemService(ConnectivityManager::class.java)
+    private lateinit var networkCallback: NetworkCallback
+
+    override fun onActive() {
+        super.onActive()
+        postValue(Event(updateConnection()))
+        connectivityManager.registerDefaultNetworkCallback(connectivityManagerCallback())
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    private fun connectivityManagerCallback(): NetworkCallback {
+        networkCallback = object : NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                postValue(Event(true))
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+                postValue(Event(false))
+            }
+        }
+        return networkCallback
+    }
+
+    private fun updateConnection(): Boolean {
+        val currentNetwork = connectivityManager.activeNetwork ?: return false
+        val caps = connectivityManager.getNetworkCapabilities(currentNetwork) ?: return false
+        val result = when {
+            caps.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            caps.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            caps.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            caps.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+            caps.hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> true
+            else -> false
+        }
+        return result
+    }
+}
