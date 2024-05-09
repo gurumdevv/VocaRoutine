9d282ff0c74f71fbbbdd951dc1d65adb87ebb74d
gurumdevv <gurumdevv@gmail.com>
2024-05-09 22:44:37 +0900

    fix: 로그인 오류 수정, uid가 DataStore에 저장되지 않던 문제 수정

--- app/src/main/java/com/gurumlab/vocaroutine/ui/login/LoginFragment.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/ui/login/LoginFragment.kt
@@ -1,6 +1,5 @@
 package com.gurumlab.vocaroutine.ui.login
 
-import android.app.Activity
 import android.content.Intent
 import android.content.IntentSender
 import android.os.Bundle
@@ -9,15 +8,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.GetSignInIntentRequest
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
@@ -33,9 +36,9 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class LoginFragment : BaseFragment<FragmentLoginBinding>() {
 
-    private val oneTapResultLauncher = getResultLauncher()
-    private lateinit var signInRequest: BeginSignInRequest
-    private lateinit var oneTapClient: SignInClient
+    private var signInRequest: BeginSignInRequest = getBeginSignInRequest()
+    private lateinit var signInClient: SignInClient
+
     @Inject
     lateinit var userDataSource: UserDataSource
 
@@ -55,25 +58,12 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
                 findNavController().navigate(action)
             }
         }
-
-        signInRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
-                    .setFilterByAuthorizedAccounts(false)
-                    .build()
-            )
-            .build()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding!!.btnGoogleSignIn.setOnClickListener {
-            setSignInRequest()
-        }
-
+        setSignInRequest()
         hideBottomNavigation(false)
     }
 
@@ -87,67 +77,154 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
             val action = LoginFragmentDirections.actionLoginToHome()
             findNavController().navigate(action)
         } else {
-            Toast.makeText(context, "로그아웃됨", Toast.LENGTH_SHORT).show()
+            Snackbar.make(requireView(), getString(R.string.logout_success), Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
     private fun setSignInRequest() {
-        oneTapClient = Identity.getSignInClient(requireActivity())
+        signInClient = Identity.getSignInClient(requireActivity())
 
-        oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener(requireActivity()) { result ->
+        val legacySignInLauncher = getLegacySignInResultLauncher()
+        val oneTapSignInLauncher = getOneTapSignInResultLauncher(legacySignInLauncher)
+        binding.btnGoogleSignIn.setOnClickListener {
+            startGoogleSignIn(oneTapSignInLauncher, legacySignInLauncher)
+        }
+    }
+
+    private fun getBeginSignInRequest() = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
+                .setFilterByAuthorizedAccounts(false)
+                .build()
+        ).build()
+
+    private fun startGoogleSignIn(
+        oneTapSignInLauncher: ActivityResultLauncher<IntentSenderRequest>,
+        legacySignInLauncher: ActivityResultLauncher<IntentSenderRequest>
+    ) {
+        signInClient.beginSignIn(signInRequest)
+            .addOnSuccessListener { result ->
                 try {
-                    oneTapResultLauncher.launch(
-                        IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
+                    oneTapSignInLauncher.launch(
+                        IntentSenderRequest
+                            .Builder(result.pendingIntent.intentSender)
+                            .build()
                     )
                 } catch (e: IntentSender.SendIntentException) {
-                    Log.e("LoginFragment", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    Log.e(tag, "sign-in error: ${e.localizedMessage}")
                 }
             }
-            .addOnFailureListener(requireActivity()) { e ->
-                Log.d("LoginFragment", "Begin sign-in request failed", e)
+            .addOnFailureListener { e ->
+                Log.e(tag, "sign-in fail: ${e.localizedMessage}")
+                requestLegacySignIn(legacySignInLauncher)
+            }
+            .addOnCanceledListener {
+                Log.e(tag, "sign-in cancelled")
             }
     }
 
-    private fun getResultLauncher() =
-        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = result.data
-                try {
-                    signInWithOneTap(data)
-                } catch (e: ApiException) {
-                    Log.e("LoginFragment", "One Tap sign-in failed", e)
-                }
+    private fun getOneTapSignInResultLauncher(
+        legacySignInLauncher: ActivityResultLauncher<IntentSenderRequest>
+    ): ActivityResultLauncher<IntentSenderRequest> {
+        return registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { activityResult ->
+            try {
+                signInWithGoogle(activityResult.data)
+            } catch (e: ApiException) {
+                Log.e("LoginFragment", "One Tap sign-in failed", e)
+                requestLegacySignIn(legacySignInLauncher)
             }
         }
+    }
 
-    private fun signInWithOneTap(data: Intent?) {
-        val credential = oneTapClient.getSignInCredentialFromIntent(data)
+    private fun requestLegacySignIn(launcher: ActivityResultLauncher<IntentSenderRequest>) {
+        val lastToken = GoogleSignIn.getLastSignedInAccount(requireContext())?.idToken
+        if (lastToken == null) {
+            val request = GetSignInIntentRequest.builder()
+                .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
+                .build()
+
+            signInClient
+                .getSignInIntent(request)
+                .addOnSuccessListener { pendingIntent ->
+                    launcher.launch(
+                        IntentSenderRequest
+                            .Builder(pendingIntent.intentSender)
+                            .build()
+                    )
+                }
+                .addOnFailureListener { e ->
+                    Log.e(tag, "Legacy sign-in fail: ${e.localizedMessage}")
+                    Snackbar.make(
+                        binding.root,
+                        getString(R.string.message_sign_in_failure),
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+                .addOnCanceledListener {
+                    Log.e(tag, "Legacy sign-in cancelled")
+                }
+        } else {
+            getFirebaseCredential(lastToken)
+        }
+    }
+
+    private fun getLegacySignInResultLauncher(): ActivityResultLauncher<IntentSenderRequest> {
+        return registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { activityResult ->
+            try {
+                signInWithGoogle(activityResult.data)
+            } catch (e: Exception) {
+                Log.e(tag, "Legacy launcher error: ${e.localizedMessage}", e)
+            }
+        }
+    }
+
+    private fun signInWithGoogle(data: Intent?) {
+        val credential = signInClient.getSignInCredentialFromIntent(data)
         val idToken = credential.googleIdToken
         if (idToken != null) {
-            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-            FirebaseAuth.getInstance().signInWithCredential(firebaseCredential)
-                .addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        val user = FirebaseAuth.getInstance().currentUser
-                        saveUid(user!!.uid)
-                        updateUI(user)
-                    } else {
-                        Log.e(
-                            "LoginFragment",
-                            "Firebase authentication failed",
-                            it.exception
-                        )
-                        updateUI(null)
-                    }
-                }
+            getFirebaseCredential(idToken)
         }
     }
 
-    private fun saveUid(uid: String) {
-        lifecycleScope.launch {
-            userDataSource.setUid(uid)
-        }
+    private fun getFirebaseCredential(idToken: String?) {
+        val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(firebaseCredential)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val user = FirebaseAuth.getInstance().currentUser
+                    val uid = user?.uid
+                    if (uid.isNullOrBlank()) {
+                        showLoginErrorSnackBar()
+                    } else {
+                        lifecycleScope.launch {
+                            userDataSource.setUid(uid)
+                            updateUI(user)
+                        }
+                    }
+                } else {
+                    Log.e(
+                        "LoginFragment",
+                        "Firebase authentication failed",
+                        it.exception
+                    )
+                    showLoginErrorSnackBar()
+                }
+            }
+    }
+
+    private fun showLoginErrorSnackBar() {
+        Snackbar.make(
+            requireView(),
+            R.string.message_sign_in_failure,
+            Snackbar.LENGTH_LONG
+        ).show()
     }
 
     private suspend fun isLogin(): Boolean {
