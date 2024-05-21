f5af834cdc0abe02b0120f5fd1351ef77b531c6e
gurumdevv <gurumdevv@gmail.com>
2024-05-21 17:28:38 +0900

    refactor: GPT prompt 변경

--- app/src/main/java/com/gurumlab/vocaroutine/data/source/repository/MakingListRepository.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/data/source/repository/MakingListRepository.kt
@@ -30,7 +30,7 @@ class MakingListRepository @Inject constructor(
                 GptVersion.CURRENT_VERSION, listOf(
                     ChatMessage(
                         "system",
-                        "\'${word}\'를 분해해서 각 의미에 대해서 100자 이내로 어원만 알려줘. 문장의 끝은 \"니다\"로 끝내줘"
+                        "\'${word}\'에 대해서 어원을 알고 싶은데 단어가 분해가 가능하면 분해해서 100자 이내로 알려주고 문장의 끝은 \"니다\"로 끝내줘"
                     )
                 )
             )
