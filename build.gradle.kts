ab0c2676901bb1ff45134622f19045bfcc754966
gurumdevv <gurumdevv@gmail.com>
2024-11-13 14:59:39 +0900

    build: 의존성 라이브러리 업데이트(Hilt)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.devtools.ksp") version "1.9.25-1.0.20" apply false
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
