c2f5aae5928369514d9a99adb9646d5620ddbdf3
gurumdevv <gurumdevv@gmail.com>
2024-11-13 14:28:05 +0900

    fix: ImageViewBindings에서 prefix 'app:' 제거

--- app/src/main/java/com/gurumlab/vocaroutine/ui/bindings/ImageViewBindings.kt
+++ app/src/main/java/com/gurumlab/vocaroutine/ui/bindings/ImageViewBindings.kt
@@ -4,7 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.gurumlab.vocaroutine.util.StampImageView
 
-@BindingAdapter(value = ["app:reviewStamp", "app:day"], requireAll = true)
+@BindingAdapter(value = ["reviewStamp", "day"], requireAll = true)
 fun setStamp(view: ImageView, isReview: Boolean, day: Int) {
     if (view is StampImageView) {
         view.setStamp(isReview, day)
